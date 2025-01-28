df376ccd78b19ebd57f6699bbb7a0b084b34ce9f
MDAshif4002 <mohdashif4002@gmail.com>
2025-01-28 11:06:29 +0530

    bv

--- src/app/support/support.component.ts
+++ src/app/support/support.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-support',
   standalone:true,
-  imports: [RouterLink,CommonModule],
+  imports: [CommonModule],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css'
 })
