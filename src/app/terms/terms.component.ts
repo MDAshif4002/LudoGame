80e6649f2978513f834e347e73ee90607d6e87e5
MDAshif4002 <mohdashif4002@gmail.com>
2025-01-28 11:03:54 +0530

    bcgh

--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
   standalone:true,
-  imports: [RouterLink,CommonModule],
+  imports: [CommonModule],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.css'
 })
