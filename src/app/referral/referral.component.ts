493a210ffd37a189fab9b3f49025917dd734f59f
MDAshif4002 <mohdashif4002@gmail.com>
2025-01-28 11:12:19 +0530

    mhb,vJ

--- src/app/referral/referral.component.ts
+++ src/app/referral/referral.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-referral',
   standalone:true,
-  imports: [RouterLink,CommonModule],
+  imports: [CommonModule],
   templateUrl: './referral.component.html',
   styleUrl: './referral.component.css'
 })
