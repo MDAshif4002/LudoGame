a6d7789b9ca6fa360f5c041e89f660bf0bf67bc9
MDAshif4002 <mohdashif4002@gmail.com>
2025-01-28 11:01:08 +0530

    jhbs

--- src/app/withdraw/withdraw.component.ts
+++ src/app/withdraw/withdraw.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-withdraw',
   standalone:true,
-  imports: [RouterLink,CommonModule],
+  imports: [CommonModule],
   templateUrl: './withdraw.component.html',
   styleUrl: './withdraw.component.css'
 })
